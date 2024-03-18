03f93613763aa0a34f18860cc77e3304a1a944db
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-18 15:10:14 +0530

    Update extraction.py

--- EDGEMUSIC/utils/extraction.py
+++ EDGEMUSIC/utils/extraction.py
@@ -1,7 +1,7 @@
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message, User
 
-from EDGEMusic import app
+from EDGEMUSIC import app
 
 
 async def extract_user(m: Message) -> User:
