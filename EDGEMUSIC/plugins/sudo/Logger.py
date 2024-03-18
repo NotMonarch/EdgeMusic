e91bcbbd5fb516f123cc9622625f7c6bd44a9072
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-18 14:59:12 +0530

    Update Logger.py

--- EDGEMUSIC/plugins/sudo/Logger.py
+++ EDGEMUSIC/plugins/sudo/Logger.py
@@ -1,9 +1,9 @@
 from pyrogram import filters
 
-from EDGEMusic import app
-from EDGEMusic.misc import SUDOERS
-from EDGEMusic.utils.database import add_off, add_on
-from EDGEMusic.utils.decorators.language import language
+from EDGEMUSIC import app
+from EDGEMUSIC.misc import SUDOERS
+from EDGEMUSIC.utils.database import add_off, add_on
+from EDGEMUSIC.utils.decorators.language import language
 
 
 @app.on_message(filters.command(["logger"]) & SUDOERS)
