66d94c64de1cfea10e262b5c58835de979aad799
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-18 14:30:23 +0530

    Update Soundcloud.py

--- EDGEMUSIC/platforms/Soundcloud.py
+++ EDGEMUSIC/platforms/Soundcloud.py
@@ -2,7 +2,7 @@ from os import path
 
 from yt_dlp import YoutubeDL
 
-from EDGEMusic.utils.formatters import seconds_to_min
+from EDGEMUSIC.utils.formatters import seconds_to_min
 
 
 class SoundAPI:
