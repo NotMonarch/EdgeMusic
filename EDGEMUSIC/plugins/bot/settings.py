44ea6141ffb80258d3a2562f8fcfe4da85f59069
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-18 14:51:41 +0530

    Update settings.py

--- EDGEMUSIC/plugins/bot/settings.py
+++ EDGEMUSIC/plugins/bot/settings.py
@@ -8,8 +8,8 @@ from pyrogram.types import (
     Message,
 )
 
-from EDGEMusic import app
-from EDGEMusic.utils.database import (
+from EDGEMUSIC import app
+from EDGEMUSIC.utils.database import (
     add_nonadmin_chat,
     get_authuser,
     get_authuser_names,
@@ -25,15 +25,15 @@ from EDGEMusic.utils.database import (
     skip_off,
     skip_on,
 )
-from EDGEMusic.utils.decorators.admins import ActualAdminCB
-from EDGEMusic.utils.decorators.language import language, languageCB
-from EDGEMusic.utils.inline.settings import (
+from EDGEMUSIC.utils.decorators.admins import ActualAdminCB
+from EDGEMUSIC.utils.decorators.language import language, languageCB
+from EDGEMUSIC.utils.inline.settings import (
     auth_users_markup,
     playmode_users_markup,
     setting_markup,
     vote_mode_markup,
 )
-from EDGEMusic.utils.inline.start import private_panel
+from EDGEMUSIC.utils.inline.start import private_panel
 from config import BANNED_USERS, OWNER_ID
 
 
