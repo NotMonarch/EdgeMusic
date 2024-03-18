5f1d57daba090b7c34bb7fab408c618f65bb3d25
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-18 14:30:51 +0530

    Update Telegram.py

--- EDGEMUSIC/platforms/Telegram.py
+++ EDGEMUSIC/platforms/Telegram.py
@@ -6,8 +6,8 @@ from typing import Union
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Voice
 
 import config
-from EDGEMusic import app
-from EDGEMusic.utils.formatters import (
+from EDGEMUSIC import app
+from EDGEMUSIC.utils.formatters import (
     check_duration,
     convert_bytes,
     get_readable_time,
