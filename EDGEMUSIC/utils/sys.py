8ccd9bd26ba0171d207b6b80cdd409abeb410401
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-18 15:12:19 +0530

    Update sys.py

--- EDGEMUSIC/utils/sys.py
+++ EDGEMUSIC/utils/sys.py
@@ -2,8 +2,8 @@ import time
 
 import psutil
 
-from EDGEMusic.misc import _boot_
-from EDGEMusic.utils.formatters import get_readable_time
+from EDGEMUSIC.misc import _boot_
+from EDGEMUSIC.utils.formatters import get_readable_time
 
 
 async def bot_sys_stats():
