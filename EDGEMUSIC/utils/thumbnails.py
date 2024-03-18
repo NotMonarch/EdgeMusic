317ab2c482b38f2458168104f7c73593b5a9e71e
Monarch <136090488+NotMonarch@users.noreply.github.com>
2024-03-18 15:12:32 +0530

    Update thumbnails.py

--- EDGEMUSIC/utils/thumbnails.py
+++ EDGEMUSIC/utils/thumbnails.py
@@ -8,7 +8,7 @@ from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 
-from EDGEMusic import app
+from EDGEMUSIC import app
 from config import YOUTUBE_IMG_URL
 
 
